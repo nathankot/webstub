764f501c849b568dce38a25523a50ee506b1ed27
Matt Green <mattgreenrocks@gmail.com>
2012-09-06 19:13:20 -0400

    Prevent gem from being bundled into archives

--- lib/httpstub.rb
+++ lib/httpstub.rb
@@ -1,7 +1,9 @@
 gem_name = File.basename(__FILE__).gsub(/\.rb$/, "")
 
 Motion::Project::App.setup do |app|
-  Dir.glob(File.join(File.dirname(__FILE__), gem_name, "**/*.rb")).each do |file|
-    app.files.unshift(file)
+  app.development do
+    Dir.glob(File.join(File.dirname(__FILE__), gem_name, "**/*.rb")).each do |file|
+      app.files.unshift(file)
+    end
   end
 end
